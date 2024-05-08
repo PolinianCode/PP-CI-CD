26afbcd85eb52fb9374a476cf4174f59129991ce
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2024-05-08 13:48:08 +0200

    Added docstrings to functions and module

--- utils.py
+++ utils.py
@@ -1,8 +1,25 @@
+"""
+Module to perfom basic math operations on two numbers
+"""
+
+
 def add ( a: int , b: int) -> int:
+    """
+    This function takes two integers and returns their sum
+    """
     return a + b
 def subtract ( a: int , b: int) -> int:
+    """
+    This function takes two integers and returns their difference
+    """
     return a - b
 def multiply ( a: int , b: int) -> int:
+    """
+    This function takes two integers and returns their product
+    """
     return a * b
 def divide ( a: int , b: int) -> float :
+    """
+    This function takes two integers and returns their division
+    """
     return a / b
